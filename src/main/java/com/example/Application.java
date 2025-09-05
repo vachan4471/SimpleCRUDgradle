21759dddb4ac5f3b717384b2ef4271c3d4e0a1e9
vachan4471 <vachanhanamanal4471@gmail.com>
2025-09-05 14:50:40 +0530

    sent the safe .yml

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,10 +1,17 @@
 package com.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();//this will be HashMap.
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         Micronaut.run(Application.class, args);
     }
+
 }
